42e05e47f82d78b3dec06230360c01fdf85eb4fa
iskenderColak <60299811+iColak@users.noreply.github.com>
2022-02-20 17:11:32 +0300

    Add files via upload

---
+++ CloserNumbers.java
@@ -0,0 +1,33 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CloserNumbers {
+    public static void main(String[] args) {
+        /*
+        Girilen Sayı : 5
+        Girilen sayıdan küçük en yakın sayı : 2
+        Girilen sayıdan büyük en yakın sayı : 12
+        */
+        Scanner scan = new Scanner(System.in);
+        int[] arr = {15, 12, 788, 1, -1, -778, 2, 0};
+        Arrays.sort(arr);
+        System.out.print("Enter a number : ");
+        int smallCloser = 0, greatCloser = 0;
+        int number = scan.nextInt();
+
+        for (int i : arr) {
+            if (i < number) {
+                smallCloser = i;
+            } else if (i > number) {
+                greatCloser = i;
+                break;
+            }
+
+        }
+        System.out.println("closest number smaller than the entered number : " + smallCloser);
+        System.out.println("Closest number greater than the entered number : " + greatCloser);
+
+    }
+}
